fa15459475008606d5575806ebe56aa111c4ec1d
Daniel Keefe <dfk@umn.edu>
2024-09-05 16:01:31 -0500

    in class progress

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -9,6 +9,9 @@ import * as gfx from 'gophergfx'
 export class ExampleApp extends gfx.GfxApp
 {   
  
+    bgColor : gfx.Color = new gfx.Color(0, 0, 0);
+    nose : gfx.Node2 | null = null;
+
     // --- Create the ExampleApp class ---
     constructor()
     {
@@ -21,12 +24,40 @@ export class ExampleApp extends gfx.GfxApp
     // --- Initialize the graphics scene ---
     createScene(): void 
     {
+        this.renderer.background = this.bgColor;
 
+        const rEye = gfx.Geometry2Factory.createRect(0.5, 0.5);
+        //rEye.position.x = 0.5;
+        //rEye.position.y = 0.5;
+        rEye.position = new gfx.Vector2(0.5, 0.5);
+        rEye.material.color = gfx.Color.BLACK;
+        this.scene.add(rEye);
+
+        const lEye = gfx.Geometry2Factory.createRect(0.5, 0.5);
+        lEye.position = new gfx.Vector2(-0.5, 0.5);
+        this.scene.add(lEye);
+
+        this.nose = gfx.Geometry2Factory.createRect(0.25, 0.25);
+        this.scene.add(this.nose);
+
+        const mouth = gfx.Geometry2Factory.createRect(1.8, 0.15);
+        mouth.position = new gfx.Vector2(0, -0.5);
+        this.scene.add(mouth);
+
+        console.log("Hello world");
     }
 
     // --- Update is called once each frame by the main graphics loop ---
     update(deltaTime: number): void 
     {
+        if (this.bgColor.g < 0.3) {
+            this.bgColor.g += 0.01;
+        }
+        this.renderer.background = this.bgColor;
+
+        if (this.nose instanceof gfx.Node2) {
+            this.nose.position.x += 0.01;
+        }
 
     }
 }
